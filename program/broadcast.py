86bbe8956b4088eaea72495dde6f296a6a66c252
newname58q <85175546+newname58q@users.noreply.github.com>
2021-11-04 20:19:51 +0300

    Update broadcast.py

--- program/broadcast.py
+++ program/broadcast.py
@@ -6,31 +6,34 @@
 import asyncio
 
 from pyrogram import Client, filters
-from pyrogram.types import Dialog, Chat, Message
-from pyrogram.errors import UserAlreadyParticipant
+from pyrogram.types import Message
 
-from driver.veez import user as USER
-from pyrogram import client as veez
+from callsmusic.callsmusic import client as veez
 from config import SUDO_USERS
+
 
 @Client.on_message(filters.command(["gcast"]))
 async def broadcast(_, message: Message):
-    sent=0
-    failed=0
+    sent = 0
+    failed = 0
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("starting broadcast...")
+        wtf = await message.reply("`ʙʀᴏᴀᴅᴄᴀꜱᴛ ʙᴀꜱʟᴀᴅɪ...`")
         if not message.reply_to_message:
-            await wtf.edit("please reply to a message to start broadcast!")
+            await wtf.edit("ʟᴜᴛꜰᴇɴ ʏᴀʏɪɴɪ ʙᴀꜱʟᴀᴛᴍᴀᴋ ɪᴄɪɴ ʙɪʀ ᴍᴇꜱᴀᴊᴀ ᴄᴇᴠᴀᴘ ᴠᴇʀɪɴ!")
             return
         lmao = message.reply_to_message.text
-        async for dialog in USER.iter_dialogs():
+        async for dialog in veez.iter_dialogs():
             try:
-                await USER.send_message(dialog.chat.id, lmao)
-                sent = sent+1
-                await wtf.edit(f"broadcasting... \n\n**sent to:** {sent} chats \n**failed in:** {failed} chats")
+                await veez.send_message(dialog.chat.id, lmao)
+                sent = sent + 1
+                await wtf.edit(
+                    f"`ʙʀᴏᴀᴅᴄᴀꜱᴛ...` \n\n**ɢᴏɴᴅᴇʀɪʟᴅɪ:** `{sent}` ᴄʜᴀᴛ \n**ʙᴀꜱᴀʀɪꜱɪᴢ:** {failed} ᴄʜᴀᴛ"
+                )
                 await asyncio.sleep(3)
             except:
-                failed=failed+1
-        await message.reply_text(f"gcast succesfully \n\n**sent to:** {sent} chats \n**failed in:** {failed} chats")
+                failed = failed + 1
+        await message.reply_text(
+            f"`ʙʀᴏᴀᴅᴄᴀꜱᴛ ʙᴀꜱᴀʀɪʟɪ` \n\n**ɢᴏɴᴅᴇʀɪʟᴅɪ:** `{sent}` ᴄʜᴀᴛ \n**ʙᴀꜱᴀʀɪꜱɪᴢ:** {failed} chats"
+        )
