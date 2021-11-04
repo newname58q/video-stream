ee6815de248c5f699ee44010cfa6a084ad96b3cc
newname58q <85175546+newname58q@users.noreply.github.com>
2021-11-04 17:46:30 +0300

    Create broadcast.py

---
+++ program/broadcast.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2021 By TaliaProject
+# Originally written by Anonim on github
+# Broadcast function
+
+
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.types import Dialog, Chat, Message
+from pyrogram.errors import UserAlreadyParticipant
+
+from pytgcalls import client as cyber
+from config import SUDO_USERS
+
+@Client.on_message(filters.command(["gcast"]))
+async def broadcast(_, message: Message):
+    sent=0
+    failed=0
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("`Yayın başlatılıyor...`")
+        if not message.reply_to_message:
+            await wtf.edit("Yayını başlatmak için lütfen bir iletiyi yanıtlayın!")
+            return
+        lmao = message.reply_to_message.text
+        async for dialog in veez.iter_dialogs():
+            try:
+                await veez.send_message(dialog.chat.id, lmao)
+                sent = sent+1
+                await wtf.edit(f"`Yayın...` \n\n**Gönderileceği yer:** `{sent}` chats \n**Başarısız oldu:** {failed} chats")
+                await asyncio.sleep(3)
+            except:
+                failed=failed+1
+        await message.reply_text(f"`Gcast başarıyla` \n\n**Gönderileceği yer:** `{sent}` chats \n**Başarısız oldu:** {failed} chats")
